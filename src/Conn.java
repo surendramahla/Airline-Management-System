4705333d90c8ad7c2cbec31190b92abca223b822
Surendra Mahla <151872044+surendramahla@users.noreply.github.com>
2025-12-27 15:09:40 +0530

    Add files via upload

---
+++ src/Conn.java
@@ -0,0 +1,18 @@
+import java.sql.*;
+
+public class Conn {
+    
+    Connection c;
+    Statement s;
+    
+    public Conn() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            c = DriverManager.getConnection("jdbc:mysql:///airlinemanagementsystem", "root", "A5#m337!");
+            s = c.createStatement();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
